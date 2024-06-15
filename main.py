81a9dd37de9364e5c452b461b11f8cc402cace21
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-06-15 17:53:19 +0200

    Categorized and added more file type support

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import os
 import shutil
 
-# Define the mapping of file extensions to directory names
-EXTENSION_DIRS = {
-    'pdf': 'PDFs',
-    'docx': 'Word Documents',
-    'doc': 'Word Documents',
-    'rar': 'RAR Files',
-    'zip': 'ZIP Files',
-    'jpg': 'Images',
-    'jpeg': 'Images',
-    'png': 'Images',
-    # Add more extensions and corresponding directories as needed
+# Organize files into categories
+file_type_folders = {
+    'PDFs': ['pdf'],
+    'Word Documents': ['docx', 'doc'],
+    'RAR Files': ['rar'],
+    'ZIP Files': ['zip'],
+    'Images': ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg'],
+    'Text Files': ['txt'],
+    'Excel Files': ['xlsx', 'xls'],
+    'PowerPoint Files': ['pptx', 'ppt'],
+    'Archives': ['7z', 'tar', 'gz'],
+    'Media': ['mp3', 'mp4', 'wav', 'avi', 'mkv', 'mov'],
+    'Install Files': ['exe', 'msi']
 }
 
 def organize_files(folder_path):
     # Create the target directories if they do not exist
-    for dir_name in EXTENSION_DIRS.values():
+    for dir_name in file_type_folders.keys():
         dir_path = os.path.join(folder_path, dir_name)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
@@ -31,7 +33,12 @@ def organize_files(folder_path):
         file_extension = filename.split('.')[-1].lower()
         
         # Determine the target directory based on the file extension
-        target_dir = EXTENSION_DIRS.get(file_extension)
+        target_dir = None
+        for dir_name, extensions in file_type_folders.items():
+            if file_extension in extensions:
+                target_dir = dir_name
+                break
+        
         if target_dir:
             target_path = os.path.join(folder_path, target_dir, filename)
             source_path = os.path.join(folder_path, filename)
